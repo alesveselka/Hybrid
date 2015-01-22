92ae94096102f4441cd8dce69aa80b9a10c6ef73
Ales Veselka <ales.veselka@gmail.com>
2015-01-22 15:57:05 +0100

    GraphicUtils drawArc

--- js/src/utils/GraphicUtils.js
+++ js/src/utils/GraphicUtils.js
@@ -41,5 +41,59 @@ App.GraphicUtils = {
         for (;i<l;) graphics.drawRect(data[i++],data[i++],data[i++],data[i++]);
 
         if (end) graphics.endFill();
+    },
+    /**
+     * Draw arc
+     * @param {PIXI.Graphics} graphics
+     * @param {PIXI.Point} center
+     * @param {number} width
+     * @param {number} height
+     * @param {number} thickness
+     * @param {number} startAngle
+     * @param {number} endAngle
+     * @param {number} smoothSteps
+     * @param {number} lineColor
+     * @param {number} lineAlpha
+     * @param {number} lineThickness
+     * @param {number} fillColor
+     * @param {number} fillAlpha
+     */
+    drawArc:function drawArc(graphics,center,width,height,thickness,startAngle,endAngle,smoothSteps,lineColor,lineAlpha,lineThickness,fillColor,fillAlpha)
+    {
+        startAngle -= 90;
+        endAngle -= 90;
+
+        var angle = startAngle,
+            degToRad = Math.PI / 180,
+            radians = angle * degToRad,
+            radiusX = width / 2,
+            radiusY = height / 2,
+            centerX = center.x,
+            centerY = center.y,
+            i = 0;
+
+        graphics.clear();
+        graphics.lineStyle(lineThickness,lineColor,lineAlpha);
+        graphics.beginFill(fillColor,fillAlpha);
+        graphics.moveTo(centerX+Math.cos(radians)*radiusX,centerY+Math.sin(radians)*radiusY);
+
+        for (;i<smoothSteps;)
+        {
+            angle = startAngle + ((endAngle - startAngle) / smoothSteps) * i++;
+            radians = angle * degToRad;
+            graphics.lineTo(centerX+Math.cos(radians)*radiusX,centerY+Math.sin(radians)*radiusY);
+        }
+
+        radians = endAngle * degToRad;
+        graphics.lineTo(centerX+Math.cos(radians)*radiusX,centerY+Math.sin(radians)*radiusY);
+
+        for (i=smoothSteps;i>=0;)
+        {
+            angle = startAngle + ((endAngle - startAngle) / smoothSteps) * i--;
+            radians = angle * degToRad;
+            graphics.lineTo(centerX+Math.cos(radians)*(radiusX-thickness),centerY+Math.sin(radians)*(radiusY-thickness));
+        }
+
+        graphics.endFill();
     }
 };
